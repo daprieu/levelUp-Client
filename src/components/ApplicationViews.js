84927b2e0312e2c46c30bb0332a3068ddcd602f9
daprieu <daprieu@gmail.com>
2021-05-10 14:33:59 -0500

    implemtented code to add games to game list with provider and form

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { EventList } from "./game/EventList.js"
 import { EventProvider } from "./game/EventProvider.js"
+import { GameForm } from "./game/GameForm.js"
 import { GameList } from "./game/GameList.js"
 import { GameProvider } from "./game/GameProvider.js"
 
@@ -12,8 +13,11 @@ export const ApplicationViews = () => {
             lineHeight: "1.75rem"
         }}>
             <GameProvider>
-                <Route exact path="/">
+                <Route exact path="/games">
                     <GameList />
+                </Route>
+                <Route exact path="/games/new">
+                    <GameForm />
                 </Route>
             </GameProvider>
             <EventProvider>
